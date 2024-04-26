e4d7fbe0e04353b3b0892fc3cb44915407d2e282
Blns159 <22022632@vnu.edu.vn>
2024-04-27 03:09:59 +0700

    front-end

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Delicious Artisanal Sandwiches Await!</h2>
+        <p>Explore a diverse menu brimming with mouthwatering sandwiches, meticulously crafted using premium ingredients and culinary flair. Our goal? To tantalize your taste buds and elevate your sandwich game, one bite at a time.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/pages/Cart/Cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Cart.css'
+
+const Cart = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+import './Cart.css'
+
+export default Cart

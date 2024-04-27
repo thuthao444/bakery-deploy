450f8ded8de3a0db7f49b7938d114c13b4f280e6
trangdo <trangdo191024@gmail.com>
2024-04-27 18:21:26 +0700

    Sign in/Sign up

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,29 +3,29 @@ import './Navbar.css'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
 
     const [menu, setMenu] = useState("home");
 
-  return (
-    <div className='navbar'>
-        <img src={assets.logo} alt="" className="logo" />
-        <ul className="navbar-menu">
-            <Link to='/' onClick={()=>setMenu("home")}className={menu==="home"?"active":""}>home</Link>
-            <a href='#explore-menu'onClick={()=>setMenu("menu")}className={menu==="menu"?"active":""}>menu</a>
-            <a href='#app-download'onClick={()=>setMenu("mobile-app")}className={menu==="mobile-app"?"active":""}>mobile app</a>
-            <a href='#footer'onClick={()=>setMenu("contact-us")}className={menu==="contact-us"?"active":""}>contact us</a>
-        </ul>
-        <div className="navbar-right">
-            <img src={assets.search_icon} alt="" className="" />
-            <div className="navbar-search-icon">
-                <img src={assets.basket_icon} alt="" className="" />
-                <div className="dot"></div>
+    return (
+        <div className='navbar'>
+            <img src={assets.logo} alt="" className="logo" />
+            <ul className="navbar-menu">
+                <Link to='/' onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>home</Link>
+                <a href='#explore-menu' onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu</a>
+                <a href='#app-download' onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>mobile app</a>
+                <a href='#footer' onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>contact us</a>
+            </ul>
+            <div className="navbar-right">
+                <img src={assets.search_icon} alt="" className="" />
+                <div className="navbar-search-icon">
+                    <img src={assets.basket_icon} alt="" className="" />
+                    <div className="dot"></div>
+                </div>
+                <button onClick={() => setShowLogin(true)}>sign in</button> // an vao sign in thi showLogin = true , hien popup
             </div>
-            <button>sign in</button>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar

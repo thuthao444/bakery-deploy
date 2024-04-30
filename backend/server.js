5294d0ed8ddc053be8619f092c53cf0d213d766a
Thao <chithaotac@gmail.com>
2024-04-30 13:22:53 +0700

    update frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import express from 'express'
 import cors from 'cors'
 import { connectDB } from './config/db.js'
 import foodRouter from './routes/foodRoute.js'
-
+import userRouter from './routes/userRoute.js'
+import 'dotenv/config'
+import cartRouter from './routes/cartRoute.js'
 
 
 // app config
@@ -16,10 +18,11 @@ app.use(cors())
 // db connection
 connectDB();
 
-// app.use('/images', express.static(path.join(__dirname, 'images')));
-
 // api endpoints
 app.use("/api/food", foodRouter)
+app.use("/images", express.static('uploads'))
+app.use("/api/user", userRouter)
+app.use("/api/cart", cartRouter)
 
 app.get("/", (req, res) => {
     res.send("API working")

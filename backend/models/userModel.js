118c372a90b7f43e22e2379e68a386e1039c85e8
Page0526 <phuongtrang2652004@gmail.com>
2024-05-03 02:21:30 +0700

    update frontend

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose"
 
-const userChema = new mongoose.Schema({
+const userSChema = new mongoose.Schema({
     name:{type:String,required:true},
     email:{type:String,required:true,unique:true},
     password:{type:String,required:true},
     cartData:{type:Object,default:{}}
 },{minimize:false})
 
-const userModel = mongoose.models.user || mongoose.model("user",userChema);
+const userModel = mongoose.models.user || mongoose.model("user",userSChema);
 export default userModel;

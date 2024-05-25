e0b02ecab110c90e32e40c19cd1518ceea908563
Thao <chithaotac@gmail.com>
2024-05-25 23:24:31 +0700

    push frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 3000, 
+    port: process.env.PORT || 5173, 
   },
 });

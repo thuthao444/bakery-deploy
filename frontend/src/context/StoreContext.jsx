2873f4134bf8a35620f88050ebb0f9b87d3f1145
Thao <chithaotac@gmail.com>
2024-05-25 21:34:28 +0700

    push

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000";
+    const url = "https://backend1-ge1g.onrender.com";
     const [token, setToken] = useState("");
     const [food_list, setFoodList] = useState([])
     const [name, setName] = useState("");
